32ee323c31cab1e7212dc2a16cf0d3f1e5c32066
dit18131 <dit18131@office365.uop.gr>
2020-03-26 23:07:08 +0200

    Declaring variables and symbolic constansts

--- Lab1_1/Source.asm
+++ Lab1_1/Source.asm
@@ -1,8 +1,11 @@
 title Laboratory 1 : Evaluating Simple Airthmetic Expressions
 Include Irvine32.inc
-
+	P EQU 8d
 .data
-
+	x SDWORD 16d
+	y SDWORD 154d
+	z SDWORD -17d
+	w SDWORD ?
 
 .code
 main PROC
