3c89c5c498ba90d7a9147c2991084e2e6dcbea60
dit18131 <dit18131@office365.uop.gr>
2020-03-26 23:27:09 +0200

    Written code to evaluate arithmetic expression

--- Lab1_1/Source.asm
+++ Lab1_1/Source.asm
@@ -9,7 +9,17 @@ Include Irvine32.inc
 
 .code
 main PROC
+	;w = (x + y) – (50d + x + y + z - p)
+	MOV eax,x ;EAX=x
+	ADD eax,y ;EAX=x+y
 
+	MOV ebx,eax ;EBX=x+y
+	ADD ebx,50 ;EBX=x+y+50
+	ADD ebx,z ;EBX=x+y50+Z
+	SUB ebx,p ;EBX=x+y50+Z-P
+	SUB eax,ebx ;EAX=(x + y) – (50d + x + y + z - p)
+	MOV w,eax ;w=(x + y) – (50d + x + y + z - p)
+	call WriteInt
 	exit
 main ENDP
 END main 
